f638724c91058aa182a2dc1db05b7a1b288bacf7
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-03 11:55:15 -0400

    rename hoc before working on render prop

---
+++ 02-hocs-render-props/exercise/src/App.start.hoc.js
@@ -0,0 +1,52 @@
+import React from "react";
+import createMediaListener from "./lib/createMediaListener";
+import { Galaxy, Trees, Earth } from "./lib/screens";
+import { CSSTransition } from "react-transition-group";
+
+
+// queries sets up a closure that contains the queries
+// and returns a function that consumes a component
+// the component then gets called with the media prop
+// passed in
+const withMedia = queries => Comp => {
+  const media = createMediaListener(queries);
+  return class WithMedia extends React.Component {
+  state = {
+    media: media.getState()
+  };
+
+  componentDidMount() {
+    media.listen(media => this.setState({ media }));
+  }
+
+  componentWillUnmount() {
+    media.dispose();
+  }
+render() {
+  // also want to pass down any props passed to this HOC
+  return <Comp media={this.state.media} {...this.props} />
+}
+  }
+}
+class App extends React.Component {
+  render() {
+    const { media } = this.props;
+
+    return (
+      <CSSTransition classNames="fade" timeout={300}>
+        {media.big ? (
+          <Galaxy key="galaxy" />
+        ) : media.tiny ? (
+          <Trees key="trees" />
+        ) : (
+          <Earth key="earth" />
+        )}
+      </CSSTransition>
+    );
+  }
+}
+
+export default withMedia({
+    big: "(min-width : 1800px)",
+    tiny: "(max-width: 1000px)"
+})(App);
